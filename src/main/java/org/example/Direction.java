339762b8339cb1188eb6a7f59ad200a537c576d5
Zinhle Hlongwane <zihlojhb025@student.wethinkcode.co.za>
2026-04-15 09:55:11 +0200

    Build the Drone class

---
+++ src/main/java/org/example/Direction.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public enum Direction {
+
+    NORTH, SOUTH, EAST, WEST
+}
---
+++ src/main/java/org/example/Drone.java
@@ -0,0 +1,107 @@
+package org.example;
+
+public class Drone {
+
+    private String name;
+    private int positionX;
+    private int positionY;
+    private String status;
+    private Direction facing;
+
+    public Drone(String name) {
+        this.name = name;
+        this.positionX = 0;
+        this.positionY = 0;
+        this.status = "Ready";
+        this.facing = Direction.NORTH;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPositionX() {
+        return this.positionX;
+    }
+
+    public int getPositionY() {
+        return this.positionY;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public Direction getFacing() {
+        return this.facing;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public void setFacing(Direction facing) {
+        this.facing = facing;
+    }
+
+    public void moveForward(int steps) {
+        switch(facing) {
+            case NORTH:
+                this.positionY += steps;
+                break;
+            case SOUTH:
+                this.positionY -= steps;
+                break;
+            case EAST:
+                this.positionX += steps;
+                break;
+            case WEST:
+                this.positionX -= steps;
+        }
+        this.status = "Moved by " + steps + ".";
+    }
+
+    public void turnLeft() {
+        switch(facing) {
+            case NORTH:
+                this.facing = Direction.WEST;
+                break;
+            case WEST:
+                this.facing = Direction.SOUTH;
+                break;
+            case SOUTH:
+                this.facing = Direction.EAST;
+                break;
+            case EAST:
+                this.facing = Direction.NORTH;
+                break;
+        }
+        this.status = "Turned left";
+    }
+
+    public void turnRight() {
+        switch(facing) {
+            case NORTH:
+                this.facing = Direction.EAST;
+                break;
+            case EAST:
+                this.facing = Direction.SOUTH;
+                break;
+            case SOUTH:
+                this.facing = Direction.WEST;
+                break;
+            case WEST:
+                this.facing = Direction.NORTH;
+                break;
+        }
+        this.status = "Turned right.";
+    }
+}
